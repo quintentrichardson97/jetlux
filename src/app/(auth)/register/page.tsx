42890f9c2dc922200029df9133f28c8beb8648ff
Quinten Richardson <qwebdevelopment@gmail.com>
2025-07-23 21:19:09 -0500

    updated registration

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -2,11 +2,6 @@
 
 import { useState } from 'react'
 import Link from 'next/link'
-import type { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'Register',
-}
 
 interface FormState {
   username: string
